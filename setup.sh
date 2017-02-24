65ca03dab4d0b12b8a4f59a3b1ce90dce2d7c06c
Isaac (Ike) Arias <ikester+github@gmail.com>
2017-02-24 16:08:13 -0500

    Update for Hypriot 1.2.1

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -e
 
-echo Script for Linux raspberrypi 4.1.19-v7+ #858
+echo Script for Linux raspberrypi Hypriot 1.2.1
 
 sudo sed -i: 's|^Exec.*toothd$| \
-ExecStart=/usr/lib/bluetooth/bluetoothd -C \
+ExecStart=/usr/lib/bluetooth/bluetoothd -C --noplugin=sap \
 ExecStartPost=/usr/bin/sdptool add SP \
-ExecStartPost=/bin/hciconfig hci0 piscan \
 |g' /lib/systemd/system/bluetooth.service
 
 sudo cat <<EOF | sudo tee /etc/systemd/system/rfcomm.service > /dev/null
@@ -15,7 +14,7 @@ After=bluetooth.service
 Requires=bluetooth.service
  
 [Service]
-ExecStart=/usr/bin/rfcomm watch hci0 1 getty rfcomm0 115200 vt100 -a pi
+ExecStart=/usr/bin/rfcomm watch hci0 1 getty rfcomm0 115200 vt100
  
 [Install]
 WantedBy=multi-user.target
@@ -23,5 +22,4 @@ EOF
 
 sudo systemctl enable rfcomm
 
-echo Pair and "screen /dev/cu.raspberrypi-SerialPort 115200"
-
+echo Pair and "screen /dev/cu.<hostname>-SerialPort 115200"
